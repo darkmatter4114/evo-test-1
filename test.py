b1fc5e9188aecd66dc2a1eff6e68e0baccaa577e
darkmatter4114 <31249386+darkmatter4114@users.noreply.github.com>
2021-12-20 23:28:24 +0200

    add file

---
+++ test.py
@@ -0,0 +1,23 @@
+def direction(facing, turn):
+    rose = {
+        "N": 0,
+        "NE": 45,
+        "E": 90,
+        "SE": 135,
+        "S": 180,
+        "SW": 225,
+        "W": 270,
+        "NW": 315
+        }
+    t = rose[facing]+turn
+    if t > 361 : t %= 360
+    for key, value in rose.items():
+        if t == value:
+            return key
+        elif t==360:
+            return 'N'
+
+print(direction("S", 180))
+print(direction("SE", -45))
+print(direction("W", 495))
+print(direction("N", 180))
